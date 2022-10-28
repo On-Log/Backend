d415192d314359112adaad6fd03b955c2034b6b1
seeunjang <wisjang1@naver.com>
2022-10-28 18:26:42 +0900

    docker setting

---
+++ src/main/java/com/nanal/backend/NanalApplication.java
@@ -0,0 +1,13 @@
+package com.nanal.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NanalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nanal/backend/NanalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nanal.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NanalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

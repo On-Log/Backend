04c656f17278970aedacd7964d7bf9dda5db2e86
ehdgh <ehdgh25@naver.com>
2022-11-07 11:18:49 +0900

    [feat] Oauth2 설정 관련 클래스 추가(#4)

---
+++ src/main/java/com/nanal/backend/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.nanal.backend.config;
+
+import com.nanal.backend.domain.oauth.CustomOAuth2UserService;
+import com.nanal.backend.domain.oauth.OAuth2SuccessHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtAuthFilter jwtAuthFilter;
+    private final CustomOAuth2UserService oAuth2UserService;
+    private final OAuth2SuccessHandler successHandler;
+    //private final AccessDeniedHandler accessDeniedHandler;
+    private final AuthenticationEntryPoint authenticationEntryPoint;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .exceptionHandling()
+                //.accessDeniedHandler(accessDeniedHandler)
+                .authenticationEntryPoint(authenticationEntryPoint)
+
+                .and()
+                .authorizeRequests().antMatchers("/token/**", "/main").permitAll().and()
+                // Swagger 접속 정보
+                .authorizeRequests().antMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                .anyRequest().authenticated()
+
+                .and()
+                .oauth2Login()
+                .successHandler(successHandler)
+                .userInfoEndpoint().userService(oAuth2UserService);
+
+        http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
---
+++ src/main/java/com/nanal/backend/domain/oauth/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.nanal.backend.domain.oauth.repository;
+
+import com.nanal.backend.domain.oauth.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
+}

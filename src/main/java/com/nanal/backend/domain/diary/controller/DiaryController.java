61ea7dac66b10e64293958664f3439f00d5a60f9
ehdgh <ehdgh25@naver.com>
2022-11-11 00:18:23 +0900

    [feat] 초기 데이터 설정(#11)

--- src/main/java/com/nanal/backend/domain/diary/controller/DiaryController.java
+++ src/main/java/com/nanal/backend/domain/diary/controller/DiaryController.java
@@ -9,8 +9,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 
 @RequiredArgsConstructor
 @RestController

477591e24489a8a48bb5df0cdb504f69102431eb
ehdgh <ehdgh25@naver.com>
2022-11-19 12:01:14 +0900

    [feat] Scheduler 적용(#40)

--- src/main/java/com/nanal/backend/domain/diary/repository/DiaryRepository.java
+++ src/main/java/com/nanal/backend/domain/diary/repository/DiaryRepository.java
@@ -2,6 +2,7 @@ package com.nanal.backend.domain.diary.repository;
 
 import com.nanal.backend.entity.Diary;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
@@ -10,16 +11,16 @@ import java.util.Optional;
 
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
 
-    //@Query(value = "SELECT write_date FROM diary WHERE member_id = :memberId AND write_date >= :firstDate AND write_date <= :lastDate;", nativeQuery = true)
-    //List<LocalDateTime> findByMemberAndWriteDate(Long memberId, LocalDate firstDate, LocalDate lastDate);
     @Query(value = "SELECT * FROM diary d WHERE d.member_id = :memberId AND d.write_date LIKE :likeFormat", nativeQuery = true)
     List<Diary> findListByMemberAndWriteDate(Long memberId, String likeFormat);
 
     @Query(value = "SELECT * FROM diary d WHERE d.member_id = :memberId AND d.write_date LIKE :likeFormat", nativeQuery = true)
     Optional<Diary> findDiaryByMemberAndWriteDate(Long memberId, String likeFormat);
 
+    @Modifying
+    @Query(value = "UPDATE diary d SET d.edit_status = false WHERE d.edit_status = :editStatus AND d.write_date LIKE :likeFormat", nativeQuery = true)
+    void updateEditStatusByWriteDate(Boolean editStatus, String likeFormat);
+
     @Query(value = "SELECT * FROM diary d WHERE d.member_id = :memberId AND (DATE(write_date) BETWEEN :firstDate AND :lastDate)", nativeQuery = true)
     List<Diary> findListByMemberAndBetweenWriteDate(Long memberId, LocalDate firstDate, LocalDate lastDate);
-
-    //List<LocalDateTime> findByMemberIdAndAllByWriteDateBetween(Long memberId, LocalDate firstDate, LocalDate lastDate);
 }

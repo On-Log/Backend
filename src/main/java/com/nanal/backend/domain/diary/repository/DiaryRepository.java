f1cb1eac68af9e953a5a8b4190094951ec2a2b34
ehdgh <ehdgh25@naver.com>
2022-11-20 11:11:56 +0900

    [feat] 스케쥴링을 통한 일기 수정 불가 기능(#40)

--- src/main/java/com/nanal/backend/domain/diary/repository/DiaryRepository.java
+++ src/main/java/com/nanal/backend/domain/diary/repository/DiaryRepository.java
@@ -1,6 +1,7 @@
 package com.nanal.backend.domain.diary.repository;
 
 import com.nanal.backend.entity.Diary;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -17,10 +18,14 @@ public interface DiaryRepository extends JpaRepository<Diary, Long> {
     @Query(value = "SELECT * FROM diary d WHERE d.member_id = :memberId AND d.write_date LIKE :likeFormat", nativeQuery = true)
     Optional<Diary> findDiaryByMemberAndWriteDate(Long memberId, String likeFormat);
 
+    @Query(value = "SELECT * FROM diary d WHERE d.member_id = :memberId AND (DATE(write_date) BETWEEN :firstDate AND :lastDate)", nativeQuery = true)
+    List<Diary> findListByMemberAndBetweenWriteDate(Long memberId, LocalDate firstDate, LocalDate lastDate);
+
     @Modifying
     @Query(value = "UPDATE diary d SET d.edit_status = false WHERE d.edit_status = :editStatus AND d.write_date LIKE :likeFormat", nativeQuery = true)
     void updateEditStatusByWriteDate(Boolean editStatus, String likeFormat);
 
-    @Query(value = "SELECT * FROM diary d WHERE d.member_id = :memberId AND (DATE(write_date) BETWEEN :firstDate AND :lastDate)", nativeQuery = true)
-    List<Diary> findListByMemberAndBetweenWriteDate(Long memberId, LocalDate firstDate, LocalDate lastDate);
+    @Modifying
+    @Query(value = "UPDATE diary d SET d.edit_status = false WHERE d.member_id IN (:member_ids) AND (DATE(write_date) BETWEEN :firstDate AND :lastDate)", nativeQuery = true)
+    void updateEditStatusByMemberAndBetweenWriteDate(@Param("member_ids") List<Long> member_ids, LocalDate firstDate, LocalDate lastDate);
 }

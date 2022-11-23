1df982dd3f39c35af6447ceadc2c0b1db79133b6
imdh <eastlake520@gmail.com>
2022-11-23 21:38:18 +0900

    [fix] LocalDateTime parsing 에러 해결(#82)

--- src/main/java/com/nanal/backend/domain/diary/dto/ReqDeleteDiaryDto.java
+++ src/main/java/com/nanal/backend/domain/diary/dto/ReqDeleteDiaryDto.java
@@ -1,11 +1,13 @@
 package com.nanal.backend.domain.diary.dto;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
 @Data
 public class ReqDeleteDiaryDto {
 
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private LocalDateTime deleteDate;
 }

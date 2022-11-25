7ed3b82bf5e855cffa8210fed03a74038d9685a3
seeunjang <wisjang1@naver.com>
2022-11-25 13:32:51 +0900

    [chore] LocalDateTime parsing 에러(#90)

--- src/main/java/com/nanal/backend/domain/retrospect/dto/ReqGetKeywordAndEmotionDto.java
+++ src/main/java/com/nanal/backend/domain/retrospect/dto/ReqGetKeywordAndEmotionDto.java
@@ -2,6 +2,7 @@ package com.nanal.backend.domain.retrospect.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -9,6 +10,7 @@ import java.time.LocalDateTime;
 public class ReqGetKeywordAndEmotionDto {
 
     //현재 시간
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     @Schema(description = "현재 날짜 및 시간" , example = "2022-11-19T05:33:42.387Z")
     private LocalDateTime currentDate;
 }

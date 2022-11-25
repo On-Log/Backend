5065cf5515113ab28fe94494836f69149e7c31a7
seeunjang <wisjang1@naver.com>
2022-11-25 15:19:30 +0900

    [refactor] 키워드 조회 API 수정(#92)

--- src/main/java/com/nanal/backend/domain/retrospect/dto/RespGetKeywordAndEmotionDto.java
+++ src/main/java/com/nanal/backend/domain/retrospect/dto/RespGetKeywordAndEmotionDto.java
@@ -1,11 +1,15 @@
 package com.nanal.backend.domain.retrospect.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.nanal.backend.domain.diary.dto.KeywordStringDto;
 import com.nanal.backend.entity.Diary;
+import com.nanal.backend.entity.Keyword;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 @AllArgsConstructor
@@ -13,17 +17,18 @@ import java.util.List;
 @Builder
 @Data
 public class RespGetKeywordAndEmotionDto {
-    private List<KeywordStringsDto> keywords;
+
+    private List<KeywordWriteDateDto> keywords;
 
     public static RespGetKeywordAndEmotionDto makeRespGetKeywordAndEmotionDto(List<Diary> diaries){
-        List<KeywordStringsDto> keywords = new ArrayList<>();
+        List<KeywordWriteDateDto> keywordList = new ArrayList<>();
         for(Diary d : diaries) {
-            KeywordStringsDto keywordStringsDto = KeywordStringsDto.makeKeywordStringsDto(d);
+            KeywordWriteDateDto keywordWriteDateDto = KeywordWriteDateDto.makeKeywordWriteDateDto(d);
 
-            keywords.add(keywordStringsDto);
+            keywordList.add(keywordWriteDateDto);
         }
         RespGetKeywordAndEmotionDto respGetKeywordAndEmotionDto = RespGetKeywordAndEmotionDto.builder()
-                .keywords(keywords)
+                .keywords(keywordList)
                 .build();
 
         return respGetKeywordAndEmotionDto;

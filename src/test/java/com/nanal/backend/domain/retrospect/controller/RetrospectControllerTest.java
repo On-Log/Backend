250e3f45e4f7cc24b6c90679f062f501a2006ec1
seeunjang <wisjang1@naver.com>
2023-01-11 01:53:56 +0900

    [feat] 회고 조회 테스트 코드 작성(#125)

--- src/test/java/com/nanal/backend/domain/retrospect/controller/RetrospectControllerTest.java
+++ src/test/java/com/nanal/backend/domain/retrospect/controller/RetrospectControllerTest.java
@@ -5,6 +5,7 @@ import com.nanal.backend.domain.retrospect.dto.RetrospectContentDto;
 import com.nanal.backend.domain.retrospect.dto.RetrospectKeywordDto;
 import com.nanal.backend.domain.retrospect.dto.req.ReqSaveRetroDto;
 import com.nanal.backend.domain.retrospect.dto.resp.RespGetInfoDto;
+import com.nanal.backend.domain.retrospect.dto.resp.RespGetRetroDto;
 import com.nanal.backend.domain.retrospect.service.RetrospectService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -131,5 +132,52 @@ public class RetrospectControllerTest extends CommonControllerTest {
                 );
     }
 
+    @Test
+    public void 회고_조회() throws Exception {
+        //given
+        String selectDate = "2023-01-24T00:00:00";
+        int week = 0;
+        List<RetrospectContentDto> retrospectContentDtos = new ArrayList<>(Arrays.asList(new RetrospectContentDto("이번주 나의 모습은 어땠나요?", "답변1"),
+                new RetrospectContentDto("다른 내 모습도 들려줄래요? 이번주에 찾은 의외의 내 모습이 있다면요?", "답변2"), new RetrospectContentDto("다음주에도 유지하고 싶은 나의 모습이 있을까요? 혹은 새롭게 찾고 싶은 나의 모습이 있다면 무엇인가요?", "답변3")));
+        List<RetrospectKeywordDto> retrospectKeywordDtos = new ArrayList<>(Arrays.asList(new RetrospectKeywordDto("키워드1", "그때 그대로 의미있었던 행복한 기억"),
+                new RetrospectKeywordDto("키워드2", "나를 힘들게 했지만 도움이 된 기억"),
+                new RetrospectKeywordDto("키워드3", "돌아보니, 다른 의미로 다가온 기억")));
+        RespGetRetroDto respGetRetroDto = new RespGetRetroDto(LocalDateTime.parse("2023-01-18T00:00:00"), retrospectContentDtos, retrospectKeywordDtos);
+        given(retrospectService.getRetro(any(), any())).willReturn(respGetRetroDto);
+
+        //when
+        ResultActions actions = mockMvc.perform(
+                get("/retrospect/view")
+                        .header("Token", "ACCESS_TOKEN")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .param("selectDate", selectDate)
+                        .param("week", String.valueOf(week))
+        );
+
+        //then
+        actions
+                .andExpect(status().isOk())
+                .andDo(
+                        restDocs.document(
+                                requestHeaders(
+                                        headerWithName("Token").description("접근 토큰")
+                                ),
+                                requestParameters(
+                                        parameterWithName("selectDate").description("선택 날짜"),
+                                        parameterWithName("week").description("회고 주차 (index로 되어있어서 1주 차는 0, 2주 차는 1 이런 식으로 보내야 함)")
+                                ),
+                                responseFields(
+                                        fieldWithPath("isSuccess").description("성공 여부"),
+                                        fieldWithPath("code").description("상태 코드"),
+                                        fieldWithPath("message").description("결과 메시지"),
+                                        fieldWithPath("result.writeDate").description("회고 작성 날짜"),
+                                        fieldWithPath("result.contents[].question").description("회고 목적별 질문"),
+                                        fieldWithPath("result.contents[].answer").description("질문에 대한 답변"),
+                                        fieldWithPath("result.keywords[].classify").description("회고 과정에서 키워드 분류 기준(감정 분리수거 기능)"),
+                                        fieldWithPath("result.keywords[].keyword").description("분류된 키워드")
+                                )
+                        )
+                );
+    }
 
 }

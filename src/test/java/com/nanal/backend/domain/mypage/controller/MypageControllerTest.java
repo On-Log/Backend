31953874a5fffa3c1149e481c865f066f9c55e6c
seeunjang <wisjang1@naver.com>
2023-01-12 21:38:04 +0900

    닉네임 변경 테스트 코드 작성

--- src/test/java/com/nanal/backend/domain/mypage/controller/MypageControllerTest.java
+++ src/test/java/com/nanal/backend/domain/mypage/controller/MypageControllerTest.java
@@ -1,11 +1,14 @@
 package com.nanal.backend.domain.mypage.controller;
 
 import com.nanal.backend.config.CommonControllerTest;
+import com.nanal.backend.domain.mypage.dto.req.ReqEditNicknameDto;
+import com.nanal.backend.domain.mypage.dto.resp.RespEditNicknameDto;
 import com.nanal.backend.domain.mypage.dto.resp.RespGetUserDto;
 import com.nanal.backend.domain.mypage.service.MypageService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.ResultActions;
 
 import java.time.LocalDate;
@@ -14,9 +17,9 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(MypageController.class)
@@ -56,4 +59,41 @@ public class MypageControllerTest extends CommonControllerTest {
                         )
                 );
     }
+
+    @Test
+    public void 닉네임_변경() throws Exception {
+        //given
+        ReqEditNicknameDto reqEditNicknameDto = new ReqEditNicknameDto("사용자 변경 닉네임");
+        RespEditNicknameDto respEditNicknameDto = new RespEditNicknameDto("사용자 변경 닉네임");
+        given(mypageService.updateNickname(any(), any())).willReturn(respEditNicknameDto);
+
+        //when
+        ResultActions actions = mockMvc.perform(
+                put("/mypage/nickname")
+                        .header("Token", "ACCESS_TOKEN")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(reqEditNicknameDto))
+        );
+
+        //then
+        actions
+                .andExpect(status().isOk())
+                .andDo(
+                        restDocs.document(
+                                requestHeaders(
+                                        headerWithName("Token").description("접근 토큰")
+                                ),
+                                requestFields(
+                                        fieldWithPath("nickname").description("변경한 닉네임")
+                                ),
+                                responseFields(
+                                        fieldWithPath("isSuccess").description("성공 여부"),
+                                        fieldWithPath("code").description("상태 코드"),
+                                        fieldWithPath("message").description("결과 메시지"),
+                                        fieldWithPath("result.userNickname").description("변경한 닉네임")
+                                )
+                        )
+                );
+    }
+
 }

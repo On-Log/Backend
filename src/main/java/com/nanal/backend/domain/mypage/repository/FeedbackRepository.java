42f321e31f8181d86eefe7fefcfd07236029ba96
imdh <eastlake520@gmail.com>
2023-01-25 13:21:58 +0900

    [feat] Querydsl JPAQueryFactory 빈 등록(#189)

--- src/main/java/com/nanal/backend/domain/mypage/repository/FeedbackRepository.java
+++ src/main/java/com/nanal/backend/domain/mypage/repository/FeedbackRepository.java
@@ -1,4 +1,4 @@
-package com.nanal.backend.domain.mypage;
+package com.nanal.backend.domain.mypage.repository;
 
 import com.nanal.backend.domain.mypage.entity.Feedback;
 import org.springframework.data.jpa.repository.JpaRepository;

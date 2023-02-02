2447ded4cded17bb4f12a8b2f9c1497ac7c1b2fa
imdh <eastlake520@gmail.com>
2023-02-02 22:55:34 +0900

    [feat] 검색 예외 추가(#236)

--- src/main/java/com/nanal/backend/domain/search/controller/SearchController.java
+++ src/main/java/com/nanal/backend/domain/search/controller/SearchController.java
@@ -3,10 +3,12 @@ package com.nanal.backend.domain.search.controller;
 import com.nanal.backend.domain.search.dto.ReqSearchDto;
 import com.nanal.backend.domain.search.dto.RespSearchDto;
 import com.nanal.backend.domain.search.service.SearchService;
+import com.nanal.backend.global.exception.BindingResultException;
 import com.nanal.backend.global.response.CommonResponse;
 import com.nanal.backend.global.security.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +21,11 @@ public class SearchController {
     private final SearchService searchService;
 
     @GetMapping("/search")
-    public CommonResponse<?> search(@AuthenticationPrincipal User user, @Valid ReqSearchDto reqSearchDto) {
+    public CommonResponse<?> search(@AuthenticationPrincipal User user,
+                                    @Valid ReqSearchDto reqSearchDto,
+                                    BindingResult bindingResult) {
+
+        if(bindingResult.hasErrors()) throw new BindingResultException(bindingResult.getFieldErrors());
 
         RespSearchDto respSearchDto = searchService.search(user.getSocialId(), reqSearchDto);
 

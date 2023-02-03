3d5f3b02035979fd931d7129c732171c685acea1
seeunjang <wisjang1@naver.com>
2023-02-03 22:41:20 +0900

    [refactor] 어노테이션 변경(#247)

--- src/main/java/com/nanal/backend/domain/retrospect/dto/req/ReqGetGoalDto.java
+++ src/main/java/com/nanal/backend/domain/retrospect/dto/req/ReqGetGoalDto.java
@@ -3,10 +3,11 @@ package com.nanal.backend.domain.retrospect.dto.req;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class ReqGetGoalDto {
 
-    @NotBlank(message = "goalIndex는 비어있을 수 없습니다.")
+    @NotNull(message = "goalIndex는 비어있을 수 없습니다.")
     private Long goalIndex;
 }

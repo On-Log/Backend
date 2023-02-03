224d801a0a4765ceb42b685e64f83f10edb0a024
seeunjang <wisjang1@naver.com>
2023-02-03 19:06:56 +0900

    [feat] 회고 체크(#242)

--- src/main/java/com/nanal/backend/domain/retrospect/dto/req/ReqCheckRetroDto.java
+++ src/main/java/com/nanal/backend/domain/retrospect/dto/req/ReqCheckRetroDto.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Builder
@@ -14,6 +15,7 @@ import java.time.LocalDateTime;
 @Data
 public class ReqCheckRetroDto {
 
+    @NotNull(message = "currentDate 는 비어있을 수 없습니다.")
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private LocalDateTime currentDate;
 

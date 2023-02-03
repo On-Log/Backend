862bbbe5ac48821f890b3c59145c97e04a906ab4
seeunjang <wisjang1@naver.com>
2023-02-03 19:03:44 +0900

    [feat] 회고 조회(#242)

--- src/main/java/com/nanal/backend/domain/retrospect/dto/req/ReqGetRetroDto.java
+++ src/main/java/com/nanal/backend/domain/retrospect/dto/req/ReqGetRetroDto.java
@@ -3,6 +3,7 @@ package com.nanal.backend.domain.retrospect.dto.req;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
@@ -14,5 +15,6 @@ public class ReqGetRetroDto {
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private LocalDateTime selectDate;
 
+    @NotBlank(message = "week 은 비어있을 수 없습니다.")
     private int week;
 }

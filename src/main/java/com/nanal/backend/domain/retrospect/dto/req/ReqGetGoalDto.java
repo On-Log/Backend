93ea0ab04a2f5a51927398b28ade97e57acd79ce
seeunjang <wisjang1@naver.com>
2023-02-03 19:05:43 +0900

    질문 + 도움말조회(#242)

--- src/main/java/com/nanal/backend/domain/retrospect/dto/req/ReqGetGoalDto.java
+++ src/main/java/com/nanal/backend/domain/retrospect/dto/req/ReqGetGoalDto.java
@@ -2,8 +2,11 @@ package com.nanal.backend.domain.retrospect.dto.req;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class ReqGetGoalDto {
 
+    @NotBlank(message = "goalIndex는 비어있을 수 없습니다.")
     private Long goalIndex;
 }

164841746a648e3d82697e470c62ba6580894439
imdh <eastlake520@gmail.com>
2023-02-07 10:56:10 +0900

    [feat] 감정어 유효성 검증을 위한 Custom Annotation 추가(#268)

--- src/main/java/com/nanal/backend/domain/diary/dto/req/KeywordEmotionDto.java
+++ src/main/java/com/nanal/backend/domain/diary/dto/req/KeywordEmotionDto.java
@@ -1,5 +1,6 @@
 package com.nanal.backend.domain.diary.dto.req;
 
+import com.nanal.backend.global.validation.ValueOfEmotion;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,6 +16,7 @@ import javax.validation.constraints.Size;
 public class KeywordEmotionDto {
 
     @NotNull(message = "emotion 은 비어있을 수 없습니다.")
+    @ValueOfEmotion(message = "올바른 감정어를 입력해주세요")
     @Size(min = 1, max = 5, message = "emotion 은 최소 1개, 최대 5개의 문자만 입력 가능합니다.")
     private String emotion;
 

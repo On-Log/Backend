232af72f6e88821a7940717993c9381aefcc50d9
imdh <eastlake520@gmail.com>
2023-02-14 10:08:39 +0900

    [refactor] Token Reissue 예외 수정(#347)

--- src/main/java/com/nanal/backend/domain/auth/exception/RefreshTokenInvalidException.java
+++ src/main/java/com/nanal/backend/domain/auth/exception/RefreshTokenInvalidException.java
@@ -1,11 +1,11 @@
 package com.nanal.backend.domain.auth.exception;
 
-import com.nanal.backend.global.exception.customexception.NanalException;
+import com.nanal.backend.global.exception.customexception.NanalAuthException;
 import com.nanal.backend.global.response.ErrorCode;
 
-public class RefreshTokenInvalidException extends NanalException {
+public class RefreshTokenInvalidException extends NanalAuthException {
 
-    public static final NanalException EXCEPTION = new RefreshTokenInvalidException();
+    public static final NanalAuthException EXCEPTION = new RefreshTokenInvalidException();
 
     private RefreshTokenInvalidException() {
         super(ErrorCode.INVALID_REFRESH_TOKEN);

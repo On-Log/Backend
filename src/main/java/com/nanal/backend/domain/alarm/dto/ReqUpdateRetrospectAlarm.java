35c0112489a84176e7e6ed02d01948f3f9d1e108
imdh <eastlake520@gmail.com>
2023-05-24 23:16:29 +0900

    [feat] Alarm API RestDoc 문서에 추가

--- src/main/java/com/nanal/backend/domain/alarm/dto/ReqUpdateRetrospectAlarm.java
+++ src/main/java/com/nanal/backend/domain/alarm/dto/ReqUpdateRetrospectAlarm.java
@@ -1,11 +1,15 @@
 package com.nanal.backend.domain.alarm.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class ReqUpdateRetrospectAlarm {
 
